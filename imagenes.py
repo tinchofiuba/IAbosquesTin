6e37bb92d918909576e89bb282e1a96f3583d0a2
Martin Sanchez <tinchofiuba@gmail.com>
2025-04-25 11:50:39 -0300

    optimizo la velocidad de copiado/pegado y agrego un docstring a la clase

--- imagenes.py
+++ imagenes.py
@@ -1,16 +1,22 @@
-'''
-1- parseador de imagenes.
-    1-a iterar entre las img con la clase ParsearImg y separar las 100 de las 200.. hasta las 800 en 8 clases diferentes
-2- crear un dataset de imagenes para cada una de las clases
-    2-a Se deberá usar la misma clase que la usada para el 1er parseo y ahí:
-        2-a-1- llevarlas a un valor cuadrado, ya sea deformandolas o cortandolas 
-        2-a-2  cortar n miniaturas de cada una de las imagenes almacenando las img en un directorio
-    2-b Una vez separadas y lsa guardas las miniaturas se selecciona el batch de manera random.
-'''
-from preTratamiento import PreTratamientoImag as ptImg
+from preTratamiento import ClasificadorImg_cada100 as Img_cada_100
 
-pretratamiento=ptImg()
-print(pretratamiento.numClases)
+clasif=Img_cada_100()
+#definir el número de clases a clasificar
+clasif.numClases=41
+
+#definir escala de clasificación. Cada esta escala se separan las clases de img
+clasif.escala=100 
+
+#path a donde están las carpetas con las img o donde están las img sueltas
+clasif.pathImg="/home/martin/repos/datasets/maderas_macro/clases_parseadas"
+
+#path a donde se guardaran las imagenes clasificadas, se ordenan en carpetas
+clasif.class_dir="/home/martin/repos/datasets/maderas_macro/imagenes" 
+
+#inicio del proceso de parseo
+clasif.parseoImg()
+
+
 
 
 
--- preTratamiento.py
+++ preTratamiento.py
@@ -1,93 +1,108 @@
-
 import os
-import cv2
 import shutil
+from concurrent.futures import ThreadPoolExecutor
 
-class PreTratamientoImag:
+class ClasificadorImg_cada100:
+    '''
+    Permite parsear imagenes tageadas como 102, 201, 307, 405 etc en clases de a 100 siempre y cuando el nombre sea COMPLETAMENTE NUMERICO-> 0123.jpg (OK) _0123A.jpg (mal)   
+    Por defautl maneja 41 clases y los path son el mismo que el script
+    Los formatos de img admitidos son .jpg y .png - aunque en se podŕia ampliar
+    el nom
+    '''
+    
     def __init__(self):
-        self.path= os.path.dirname(os.path.abspath(__file__)) # en caso de no asignar un path
-        self.imgList=[]
-        self.escalaClases=100 # por default
-        self.class_dir=os.path.join(self.path, "clases") # en caso de no asignar un path
-        self.numClases=41 #por default
-        self.absClassDir=None
+        self.__path = os.path.dirname(os.path.abspath(__file__))  # Ruta predeterminada
+        self.imgList = []
+        self.escalaClases = 100  # Por default
+        self.class_dir = os.path.join(self.__path, "clases")  # Carpeta de clases
+        self.numClases = 41  # Número de clases
+        self.absClassDir = None
+        self.listaErrores = []
 
     ###########
-    #setters
+    # Setters
     ###########
     @property
-    def pathImg(self)-> str:
-        return self.path
-    
+    def pathImg(self) -> str:
+        return self.__path
+
     @pathImg.setter
     def pathImg(self, path: str):
-        self.path=path
+        self.__path = path
+
     @property
-    def escala(self)->int:
+    def escala(self) -> int:
         return self.escalaClases
-    
+
     @escala.setter
-    def escala(self, escala:int):
-        if escala>0:
-            self.escalaClases=escala
+    def escala(self, escala: int):
+        if escala > 0:
+            self.escalaClases = escala
         else:
-            print("la escala no puede ser menor a 0")
-    @property        
-    def pathClass(self)->str:
+            print("La escala no puede ser menor a 0")
+
+    @property
+    def pathClass(self) -> str:
         return self.class_dir
-    
+
     @pathClass.setter
     def pathClass(self, path: str):
-        self.class_dir=path
-        
+        self.class_dir = path
+
     @property
-    def cantidadClases(self)->int:
+    def cantidadClases(self) -> int:
         return self.numClases
-    
+
     @cantidadClases.setter
-    def cantidadClases(self, num:int):
-        if num>0:
-            self.numClases=num
+    def cantidadClases(self, num: int):
+        if num > 0:
+            self.numClases = num + 1
         else:
-            print("el numero de clases no puede ser menor a 0")
+            print("El número de clases no puede ser menor a 0")
+
     ##########
-    #lo otro
+    # Métodos NO setters
     ##########
-    
-    def cortadoImg(self): #abre, corta y modifica la img
-        print("cortando")
-        
+    def cortadoImg(self):  # Abre, corta y modifica la img
+        print("Cortando")
+
     def guardarImg(self, *args, **kwargs):
-        print("guardando")
-        
+        print("Guardando")
+
+    def procesar_archivo(self, file, pathdir):
+        """Procesa un archivo individual."""
+        if file.endswith('.jpg') or file.endswith('.png') or file.endswith('.JPG') or file.endswith('.PNG'):
+            fileName = file.split(".")[0]
+            if not fileName.isdigit():  # Verificar que el nombre sea numérico
+                print(f"El archivo {file} no es numérico!!")
+                return
+            clase = int(fileName) // 100  # Número de clase
+            if clase > self.numClases or clase < 1:  # Verificar rango de clases
+                print(f"El archivo {file} no pertenece a ninguna clase entre 1 y {self.numClases}")
+                return
+            absSaveDir = os.path.join(self.class_dir, f"clase_{clase}")  # Carpeta de destino
+            if not os.path.exists(absSaveDir):
+                os.makedirs(absSaveDir)  # Crear carpeta si no existe
+            origen = os.path.join(pathdir, file)
+            destino = os.path.join(absSaveDir, file)
+            shutil.copy(origen, destino)  # Copiar archivo
+            print(f"Archivo {file} copiado a {absSaveDir}")
+
     def parseoImg(self):
+        """Paraleliza el procesamiento de imágenes usando ThreadPoolExecutor."""
         self.crearCarpeta()
-        #iterno en el path carpeta a carpeta
-        for dir in os.listdir(self.path):
-            self.pathdir=os.path.join(self.path, dir) #direccion absoluta de la carpeta
-            for file in os.listdir(self.pathdir):
-                if file.endswith('.jpg') or file.endswith('.png'):
-                    fileName=file.split(".")[0] 
-                    if not fileName.isdigit(): #me fijo que solamente haya caracteres numericos en el archivo
-                        print(f"El archivo {file} no es numerico!!")
-                        continue
-                    clase=int(fileName)//100 #numero de clase
-                    if clase>self.numClases or clase<1: #me fijo que la clase no sea mayor a la cantidad de clases
-                        print(f"El archivo {file} no pertenece a ninguna clase entre 1 y 42")
-                        continue
-                    self.absSaveDir = os.path.join(self.class_dir, f"clase_{clase}") #direccion absoluta de guardaro
-                    shutil.copy(os.path.join(self.pathdir, file), self.absSaveDir) #copio el archivo a la carpeta correspondiente
-    
+        with ThreadPoolExecutor() as executor:
+            for dir in os.listdir(self.__path):
+                pathdir = os.path.join(self.__path, dir)  # Dirección absoluta de la carpeta
+                if os.path.isdir(pathdir):  # Asegurarse de que sea un directorio
+                    for file in os.listdir(pathdir):
+                        executor.submit(self.procesar_archivo, file, pathdir)
+
     def crearCarpeta(self):
+        """Crea las carpetas para las clases."""
         for i in range(1, self.numClases): 
             self.absClassDir = os.path.join(self.class_dir, f"clase_{i}")
             if not os.path.exists(self.absClassDir):
                 os.makedirs(self.absClassDir)
             else:
-                print(f"La carpeta {self.absClassDir} ya existe.") 
-    
-    def cargarImagenes(self):
-        for root, dirs, files in os.walk(self.path):
-            for file in files:
-                if file.endswith('.jpg') or file.endswith('.png'):
-                    self.imagenes.append(os.path.join(root, file))
+                print(f"La carpeta {self.absClassDir} ya existe.")
